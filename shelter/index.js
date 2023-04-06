3f0df105a440c290f4b155649dcb20fd6f3096d1
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-04-06 22:53:01 +0400

    feat: add styles for pop-up on desctop version shelter page, add stop scroll for page when pop-up open and add overlay

--- shelter/index.js
+++ shelter/index.js
@@ -1,11 +1,15 @@
 // console.log("Итоговая оценка: 100; Вёрстка страницы Main соответствует макету при ширине экрана 1280px: +14\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Main соответствует макету при ширине экрана 768px: +14\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Main соответствует макету при ширине экрана 320px: +14\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 768px: +6\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 320px: +6\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20\nнет полосы прокрутки при ширине страницы Main от 1280рх до 768рх: +5\nнет полосы прокрутки при ширине страницы Main от 768рх до 320рх: +5\nнет полосы прокрутки при ширине страницы Pets от 1280рх до 768рх: +5\nнет полосы прокрутки при ширине страницы Pets от 768рх до 320рх: +5\nВерстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (Примеры неправильной и правильной реализации): +8\nна странице Main: +4\nна странице Pets: +4\nПри ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4\nВерстка обеих страниц валидная: +8");
 
+import json from './pets.json' assert { type: 'json'};
 /*Burger*/
 
 const menuButton = document.getElementById('menu');
 const header = document.querySelector('header');
 const menu = document.querySelector('.shelter_menu');
 const body = document.querySelector('body');
+const pet = document.querySelectorAll('.pet_info');
+const popUp = document.querySelector('.pop_up__pet');
+const html = document.querySelector('html');
 
 menuButton.addEventListener('click', function(event) {
     header.classList.toggle('open');
@@ -22,3 +26,13 @@ document.body.addEventListener('click', function(event) {
         body.classList.remove('background');
     }
 });
+
+/*POP UP*/
+console.log(pet);
+pet.forEach(item => {
+    item.addEventListener('click', function() {
+        popUp.classList.toggle('opened');
+        body.classList.toggle('background');
+        html.classList.toggle('stop')
+    })
+})
