3a9d89fa11050f637198b11c648fe21c817459cd
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-04-07 00:19:05 +0400

    feat: add creat pop-up with information about a particular pet

--- shelter/index.js
+++ shelter/index.js
@@ -11,6 +11,14 @@ const pet = document.querySelectorAll('.pet_info');
 const popUp = document.querySelector('.pop_up__pet');
 const html = document.querySelector('html');
 const popUpClose = document.querySelector('.pop_up__close');
+const popUpImg = document.querySelector('.pop_up__img');
+const petName = document.querySelector('.pop_up__title');
+const petIs = document.querySelector('.pop_up__subtitle');
+const petInfo = document.querySelector('.pop_up__description');
+const petAge = document.querySelector('.age');
+const petInoculations = document.querySelector('.inoculations');
+const petDiseases = document.querySelector('.diseases');
+const petParasites = document.querySelector('.parasites');
 
 menuButton.addEventListener('click', function(event) {
     header.classList.toggle('open');
@@ -29,13 +37,27 @@ document.body.addEventListener('click', function(event) {
 });
 
 /*POP UP*/
+
+const petOpen = (event, item) => {
+    popUp.classList.toggle("opened");
+    body.classList.toggle("background");
+    html.classList.toggle("stop");
+    event.stopPropagation();
+
+    console.log(item.id);
+    const currentPet = json.find(elem => elem.name.toLowerCase() === item.id);
+    popUpImg.src = currentPet.img;
+    petName.textContent = currentPet.name;
+    petIs.textContent = `${currentPet.type}: ${currentPet.breed}`;
+    petInfo.textContent = currentPet.description;
+    petAge.textContent = ' ' + currentPet.age;
+    petInoculations.textContent = ' ' + currentPet.inoculations;
+    petDiseases.textContent = ' ' + currentPet.diseases;
+    petParasites.textContent = ' ' + currentPet.parasites;
+}
+
 pet.forEach(item => {
-    item.addEventListener('click', function(event) {
-        popUp.classList.toggle('opened');
-        body.classList.toggle('background');
-        html.classList.toggle('stop');
-        event.stopPropagation();
-    })
+    item.addEventListener('click', (event) => petOpen(event, item));
 })
 
 document.body.addEventListener('click', function() {
